7df5636045b99614cd2c4d7fa0c356114d747077
Samuel Jahnke <asamwow@github.com>
2020-09-13 15:42:55 -0400

    change layout

--- keyboards/planck/keymaps/samuel/keymap.c
+++ keyboards/planck/keymaps/samuel/keymap.c
@@ -59,7 +59,7 @@ KC_T_LGUI, KC_T_LALT, KC_UP, KC_DOWN, KC_SPC, KC_EQL, KC_GRAVE, KC_SPC, KC_LEFT,
 ),
 [_RISE] = LAYOUT_planck_grid(
 KC_TRANSPARENT, KC_EZUNDO, KC_EZCOPY, KC_EZCUT, KC_EZPSTE, KC_INS, KC_ASTR, KC_7, KC_8, KC_9, KC_TRANSPARENT, KC_TRANSPARENT,
-KC_EZSHFT, KC_ESC, KC_CAPS, KC_PGUP, KC_PGDN, KC_HOME, KC_AMPR, KC_4, KC_5, KC_6, KC_0, KC_EZSHFT,
+KC_EZSHFT, KC_DEL, KC_CAPS, KC_PGUP, KC_PGDN, KC_HOME, KC_AMPR, KC_4, KC_5, KC_6, KC_0, KC_EZSHFT,
 KC_EZCTRL, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_1, KC_2, KC_3, KC_COM, KC_EZCTRL,
 KC_EZGUI, KC_EZALT, KC_EZUP, KC_EZDOWN, KC_SPC, KC_PIPE, KC_BSLASH, KC_END, KC_EZLEFT, KC_EZRGHT, KC_EZALT, KC_EZGUI
 ),
@@ -141,49 +141,49 @@ break;
 
 case KC_T_LALT:
 	if (record->event.pressed) {
-	   mod_press(KC_LPRN, KC_LALT, 0);
+	   mod_press(KC_LBRACKET, KC_LALT, 0);
 	} else {
-	   mod_lift(KC_LPRN, KC_LALT, 0);
+	   mod_lift(KC_LBRACKET, KC_LALT, 0);
 	}
 	return false;
 	break;
 case KC_T_RALT:
 	if (record->event.pressed) {
-	   mod_press(KC_RPRN, KC_RALT, 1);
+	   mod_press(KC_RBRACKET, KC_RALT, 1);
 	} else {
-	   mod_lift(KC_RPRN, KC_RALT, 1);
+	   mod_lift(KC_RBRACKET, KC_RALT, 1);
 	}
 	return false;
 	break;
 case KC_T_LGUI:
 	if (record->event.pressed) {
-	   mod_press(KC_SLASH, KC_LGUI, 2);
+	   mod_press(KC_LPRN, KC_LGUI, 2);
 	} else {
-	   mod_lift(KC_SLASH, KC_LGUI, 2);
+	   mod_lift(KC_LPRN, KC_LGUI, 2);
 	}
 	return false;
 	break;
 case KC_T_RGUI:
 	if (record->event.pressed) {
-	   mod_press(KC_MINS, KC_RGUI, 3);
+	   mod_press(KC_RPRN, KC_RGUI, 3);
 	} else {
-	   mod_lift(KC_MINS, KC_RGUI, 3);
+	   mod_lift(KC_RPRN, KC_RGUI, 3);
 	}
 	return false;
 	break;
 case KC_T_LCTL:
 	if (record->event.pressed) {
-	   mod_press(KC_LBRACKET, KC_LCTL, 4);
+	   mod_press(KC_BSPC, KC_LCTL, 4);
 	} else {
-	   mod_lift(KC_LBRACKET, KC_LCTL, 4);
+	   mod_lift(KC_BSPC, KC_LCTL, 4);
 	}
 	return false;
 	break;
 case KC_T_RCTL:
 	if (record->event.pressed) {
-	   mod_press(KC_RBRACKET, KC_RCTL, 5);
+	   mod_press(KC_MINS, KC_RCTL, 5);
 	} else {
-	   mod_lift(KC_RBRACKET, KC_RCTL, 5);
+	   mod_lift(KC_MINS, KC_RCTL, 5);
 	}
 	return false;
 	break;
@@ -212,7 +212,7 @@ if (record->event.pressed) {
 } else {
     layer_off(_RISE);
     if (last_mod == 8 && timer_elapsed(tap_timer) < TAPPING_TERM) {
-        tap_code16(KC_DELETE);
+        tap_code16(KC_ESC);
         last_mod = 10;
     }
 }
@@ -226,7 +226,7 @@ if (record->event.pressed) {
 } else {
     layer_off(_RISE);
     if (last_mod == 9 && timer_elapsed(tap_timer) < TAPPING_TERM) {
-        tap_code16(KC_BSPACE);
+        tap_code16(KC_SLASH);
         last_mod = 10;
     }
 }
